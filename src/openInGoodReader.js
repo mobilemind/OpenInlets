942a2d0d82f1de000c2a37c64535621eef31c491
Tom King <mobilemind@pobox.com>
2014-08-08 18:52:09 -0700

    src/openIn1Password.js, src/openInGoodReader.js: add spaces so source is more readable

--- src/openInGoodReader.js
+++ src/openInGoodReader.js
@@ -1,4 +1,4 @@
 /* jshint strict: true */
 // verify UserAgent is iOS *and* URL is http: or https: *and* file is PDF
 // then swap in URL protocol for GoodReader iOS app
-if (/iP(.d|hone)/.test(navigator.userAgent) && /https?:/.test(location.protocol) && /\.pdf($|\?)/.test(location.href)) location.href='gr'+location.href;
+if (/iP(.d|hone)/.test(navigator.userAgent) && /https?:/.test(location.protocol) && /\.pdf($|\?)/.test(location.href)) location.href = 'gr' + location.href;
