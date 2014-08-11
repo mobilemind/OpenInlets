d8d2f2fcd1fefc95b2d2c4316032d2beb6e0213f
Tom King <mobilemind@pobox.com>
2014-08-10 19:14:24 -0700

    src/openinioctocat.js: add check for iOS device

--- src/openinioctocat.js
+++ src/openinioctocat.js
@@ -1,4 +1,4 @@
 /* jshint strict: true */
-// verify host is 'github.com'
+// verify host is 'github.com' & on an iOS device
 // then swap in URL protocol for iOctocat iOS app
-if (location.host === 'github.com') location.href = location.href.replace('https:', 'ioc:');
+if (location.host === 'github.com' && /iP(.d|hone)/.test(navigator.userAgent)) location.href = location.href.replace('https:', 'ioc:');
