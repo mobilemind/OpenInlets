a412990132c591c74dc3eafb83581e05e5e5881f
Tom King <mobilemind@pobox.com>
2014-08-10 20:12:21 -0700

    src/openingooglemaps.js: update comment

--- src/openingooglemaps.js
+++ src/openingooglemaps.js
@@ -1,4 +1,4 @@
 /* jshint strict: true */
-// verify current url looks like Google Maps URL *and* has query String
+// verify current url looks like Google Maps URL *and* has query String *and* iOS device
 // then swap in URL protocol for Google Maps iOS app
 if ('maps.google.com' == location.hostname && location.search && /iP(.d|hone)/.test(navigator.userAgent)) location.href = 'comgooglemaps://' + location.search;
