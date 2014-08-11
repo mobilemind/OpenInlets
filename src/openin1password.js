24055fcb83ceddd2bbcc89fc0a739af934a31f7a
Tom King <mobilemind@pobox.com>
2014-08-10 20:15:04 -0700

    src/openin1password.js: fix missing ")" around "if" condition

--- src/openin1password.js
+++ src/openin1password.js
@@ -1,3 +1,3 @@
 /* jshint strict: true */
 // verify UserAgent is iOS & then swap in URL protocol for 1Password iOS app
-if (/iP(.d|hone)/.test(navigator.userAgent) location.href = 'op' + location.href;
+if (/iP(.d|hone)/.test(navigator.userAgent)) location.href = 'op' + location.href;
