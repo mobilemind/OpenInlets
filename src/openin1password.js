116c5dd7d7bf0e5a805305013cfadbae1596977e
Tom King <mobilemind@pobox.com>
2014-08-10 19:16:40 -0700

    src/openin1password.js, src/openingoodreader.js: remove uneeded check for "http:" or "https:"

--- src/openin1password.js
+++ src/openin1password.js
@@ -1,4 +1,3 @@
 /* jshint strict: true */
-// verify UserAgent is iOS *and* URL is http: or https:
-// then swap in URL protocol for 1Password iOS app
-if (/iP(.d|hone)/.test(navigator.userAgent) && /https?:/.test(location.protocol)) location.href = 'op' + location.href;
+// verify UserAgent is iOS & then swap in URL protocol for 1Password iOS app
+if (/iP(.d|hone)/.test(navigator.userAgent) location.href = 'op' + location.href;
