13eb225b46f7e28eb79ce3f1e8e4432717878270
Tom King <mobilemind@pobox.com>
2014-08-13 10:58:11 -0700

    index.html: initial push using README.md -> index.html; remove extraneous files; v0.0.5

--- src/openingoodreader.js
+++
@@ -1,6 +0,0 @@
-// verify UserAgent is iOS *and* file is PDF
-// then swap in URL protocol for GoodReader iOS app
-function openingoodreader() {
-  if (/iP(.d|hone)/.test(navigator.userAgent) && /\.pdf($|\?)/.test(location.href)) return location.href = 'gr' + location.href;
-}
-openingoodreader();
--- src/openinioctocat.js
+++
@@ -1,6 +0,0 @@
-// verify host is 'github.com' & on an iOS device
-// then swap in URL protocol for iOctocat iOS app
-function openinioctocat() {
-  if (location.host === 'github.com' && /iP(.d|hone)/.test(navigator.userAgent)) return location.href = location.href.replace('https:', 'ioc:');
-}
-openinioctocat();
