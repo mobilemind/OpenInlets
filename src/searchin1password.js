f5124d27260ff5ec53fda24a2d5584b0e4611c0c
Tom King <mobilemind@pobox.com>
2017-03-03 23:03:52 -0800

    src/searchin1password.js: spaces around subtraction operator

--- src/searchin1password.js
+++ src/searchin1password.js
@@ -2,6 +2,6 @@
 function searchin1password() {
   // split location.hostname into segments so we can use only last 2 segments ("domain")
   const s = location.hostname.split('.');
-  return location.href = 'onepassword4://search/' + s.slice(s.length-2).join('.');
+  return location.href = 'onepassword4://search/' + s.slice(s.length - 2).join('.');
 }
 searchin1password();
