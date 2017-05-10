025155a70f659204c12d6d085883a073f691d5a4
Tom King <mobilemind@pobox.com>
2017-05-09 17:01:48 -0700

    src/*.js: style changes (4 space indents)

--- src/openin1password.js
+++ src/openin1password.js
@@ -1,8 +1,8 @@
 // verify UserAgent is iOS & then swap in URL protocol for 1Password iOS app
 function openin1password() {
-  if (/iP(.d|hone)/.test(navigator.userAgent)) {
-    return location.href = 'op' + location.href;
-  }
-  return void 0;
+    if (/iP(.d|hone)/.test(navigator.userAgent)) {
+        return location.href = 'op' + location.href;
+    }
+    return void 0;
 }
 openin1password();
--- src/openinblogsy.js
+++ src/openinblogsy.js
@@ -2,9 +2,9 @@
 // http://blogsyapp.com/developers/
 // verify UserAgent is iOS and then swap in URL protocol for Blogsy iOS app
 function openinblogsy() {
-  if (/iP(.d|hone)/.test(navigator.userAgent)) {
-    return location.href = 'blogsy:' + location.href;
-  }
-  return void 0;
+    if (/iP(.d|hone)/.test(navigator.userAgent)) {
+        return location.href = 'blogsy:' + location.href;
+    }
+    return void 0;
 }
 openinblogsy();
