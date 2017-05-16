a0625a29f3c3184583a697ff6f7fafadf9cbc2b0
Tom King <mobilemind@pobox.com>
2017-05-15 20:42:56 -0700

    src/searchin1password.js: drop "const" by inlining "location.hostname.split(".")"

--- src/searchin1password.js
+++ src/searchin1password.js
@@ -1,7 +1,11 @@
 // open search in 1Password, using domain
 function searchin1password() {
-  // split location.hostname into segments so we can use only last 2 segments ("domain")
-  const s = location.hostname.split('.');
-  return location.href = 'onepassword4://search/' + s.slice(s.length - 2).join('.');
+  // use only last 2 segments of hostname (eg, the "domain")
+  return location.href = 'onepassword4://search/' +
+    location.hostname.
+      split('.').
+      slice(location.hostname.
+        split('.').length - 2).
+      join('.');
 }
 searchin1password();
