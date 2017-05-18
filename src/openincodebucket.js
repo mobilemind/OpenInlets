fd75a786c6242e562b507146b9a687ebc30f2de3
Tom King <mobilemind@pobox.com>
2017-05-17 20:08:02 -0700

    src/openincode*.js: initial commit

---
+++ src/openincodebucket.js
@@ -0,0 +1,13 @@
+// verify host is 'bitbucket.org' & on an iOS device
+// then swap in URL protocol for CodeBucket
+function openincodebucket() {
+  if (/iP(.d|hone)/.test(navigator.userAgent) &&
+    location.host === 'bitbucket.org') {
+    return location.href = 'codebucket://' + location.href.
+      split('/').
+      slice(2,5).
+      join('/');
+  }
+  return void 0;
+}
+openincodebucket();
---
+++ src/openincodehub.js
@@ -0,0 +1,13 @@
+// verify host is 'github.com' & on an iOS device
+// then swap in URL protocol for CodeHub
+function openincodehub() {
+  if (/iP(.d|hone)/.test(navigator.userAgent) &&
+    location.host === 'github.com') {
+    return location.href = 'codehub://' + location.href.
+      split('/').
+      slice(2,5).
+      join('/');
+  }
+  return void 0;
+}
+openincodehub();
