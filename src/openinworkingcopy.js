d169d4226cb4b8c50a9c55aff0efe1ed05685644
Tom King <mobilemind@pobox.com>
2017-05-17 20:08:50 -0700

    src/openinworkingcopy.js: update to handle deeper urls

--- src/openinworkingcopy.js
+++ src/openinworkingcopy.js
@@ -1,9 +1,13 @@
-// verify host is 'github.com' & on an iOS device
-// then swap in URL protocol for Working Copu call to show (clone if necessary)
+// verify host is 'github.com' (or 'bitbucket.org') & on an iOS device
+// then prefix URL protocol for Working Copy call to show (clone if necessary)
 function openinworkingcopy() {
   if (/iP(.d|hone)/.test(navigator.userAgent) &&
     (location.host === 'bitbucket.org' || location.host === 'github.com')) {
-    return location.href = "working-copy://show?remote=" + location.href + ".git";
+    return location.href = "working-copy://show?remote=" +
+      location.href.
+      split('/').
+      slice(0,5).
+      join('/') + ".git";
   }
   return void 0;
 }
