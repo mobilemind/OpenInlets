ac8d2853b68e6a2af79d853538e4f5f44b54d4b6
Tom King <mobilemind@pobox.com>
2017-05-18 12:37:02 -0700

    fix to work with Github gists (resolves issue #1)

--- src/openinioctocat.js
+++ src/openinioctocat.js
@@ -1,7 +1,8 @@
 // verify host is 'github.com' & on an iOS device
 // then swap in URL protocol of iOctocat iOS app
 function openinioctocat() {
-  if (location.host === 'github.com' && /iP(.d|hone)/.test(navigator.userAgent)) {
+  if (/(gist\.)?github\.com/.test(location.host) &&
+    /iP(.d|hone)/.test(navigator.userAgent)) {
     return location.href = location.href.replace('https:', 'ioc:');
   }
   return void 0;
