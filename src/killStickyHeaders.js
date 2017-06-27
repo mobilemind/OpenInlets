eba008a9319e058854b4d71fefb3e247f8d3ac33
Tom King <mobilemind@pobox.com>
2017-06-27 15:07:16 -0700

    src/killStickyHeaders.js: initial commit (based on https://alisdair.mcdiarmid.org/kill-sticky-headers/)

---
+++ src/killStickyHeaders.js
@@ -0,0 +1,13 @@
+// use querySelectorAll() to find & delete all fixed position elements of body
+function killstickyheaders() {
+  /* eslint no-var: 0, security/detect-object-injection: 0 */
+  var elements = document.querySelectorAll('body *'),
+    i = 0;
+  for (i = 0; i < elements.length; i++) {
+    if ('fixed' === getComputedStyle(elements[i]).position) {
+      elements[i].parentNode.removeChild(elements[i]);
+    }
+  }
+  return void 0;
+}
+killstickyheaders();
