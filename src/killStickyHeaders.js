9923f02f36991bf0e6f09c81004c906d612c9493
Tom King <mobilemind@pobox.com>
2017-06-27 15:14:44 -0700

    src/killStickyHeaders.js: add eslint comment to disable "no-plusplus"

--- src/killStickyHeaders.js
+++ src/killStickyHeaders.js
@@ -1,6 +1,6 @@
 // use querySelectorAll() to find & delete all fixed position elements of body
 function killstickyheaders() {
-  /* eslint no-var: 0, security/detect-object-injection: 0 */
+  /* eslint no-plusplus: 0, no-var: 0, security/detect-object-injection: 0 */
   var elements = document.querySelectorAll('body *'),
     i = 0;
   for (i = 0; i < elements.length; i++) {
