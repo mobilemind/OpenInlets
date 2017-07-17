7ff08e71e0c1126eeabc186c526d1d071f592f22
Tom King <mobilemind@pobox.com>
2017-07-17 14:15:45 -0700

    src/utmstrip.js: initial commit of UtmStrip

---
+++ src/utmstrip.js
@@ -0,0 +1,13 @@
+(() => {
+  const e = window.location.href,
+    t = e.indexOf("?");
+  if (e.indexOf("utm_") > t) {
+    let n = e.replace(/([?&]utm_(source|medium|term|campaign|content)=[^&#]+)/ig, "");
+    if (n.charAt(t) === "&") {
+      n = n.substr(0, t) + "?" + n.substr(t + 1);
+    }
+    if (n !== e) {
+      history.replaceState(null, null, n);
+    }
+  }
+})();
