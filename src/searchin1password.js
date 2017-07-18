795ea009d98f76a08d7ba3482fbc57d4206f7004
Tom King <mobilemind@pobox.com>
2017-07-18 16:21:06 -0700

    src/searchin1password.js: use "host" instead of "hostname"

--- src/searchin1password.js
+++ src/searchin1password.js
@@ -1,2 +1,2 @@
-// open search in 1Password, using only last 2 segments of hostname
+// open search in 1Password, using only last 2 segments of host
 location.href = `onepassword4://search/${location.host.split('.').slice(location.host.split('.').length - 2).join('.')}`;
