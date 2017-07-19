60855b8e2dd6a95a84eaaa9e2c59dc52b606c58d
Tom King <mobilemind@pobox.com>
2017-07-19 15:24:07 -0700

    web/openinbrave.js: republish corrected version (1.0.1) w/proper URL scheme

--- web/openinbrave.js
+++ web/openinbrave.js
@@ -1,1 +1,1 @@
-javascript:if(/iP(.d%7Chone)/.test(navigator.userAgent))%7Blocation.href=location.href.replace(/%5Ehttps?/,%22brave%22)%7Dvoid'1.0.0'
+javascript:if(/iP(.d%7Chone)/.test(navigator.userAgent))%7Blocation.href=%22brave://open-url?url=%22+encodeURIComponent(location.href)%7Dvoid'1.0.1'
