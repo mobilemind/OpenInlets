ac449932970543e77dfcfe3742422cd0134ab887
Tom King <mobilemind@pobox.com>
2017-07-18 22:55:24 -0700

    src/openingooglemaps.js: accomodate www.google.com/maps, etc.

--- src/openingooglemaps.js
+++ src/openingooglemaps.js
@@ -2,10 +2,11 @@
 // if URL has query string use query with URL protocol of Google Maps iOS app
 // else if document title looks like a place then use Google Maps iOS URL protocol
 // with title as a query to Google Maps iOS app
-if ('maps.google.com' === location.host && /iP(.d|hone)/.test(navigator.userAgent)) {
+if (/\.google\.com/.test(location.host) && /iP(.d|hone)/.test(navigator.userAgent)) {
   if (location.search) {
-    location.href = `comgooglemaps://${location.search}`;
+    location.href = 'comgooglemaps://' + location.search;
   } else if (/ - Google Maps/.test(document.title)) {
-    location.href = `comgooglemaps://?q=${encodeURI(document.title.replace(' - Google Maps', '').replace(/ /g, '+'))}`;
+    location.href = 'comgooglemaps://?q=' +
+      encodeURI(document.title.replace(' - Google Maps', '').replace(/ /g, '+'));
   }
 }
