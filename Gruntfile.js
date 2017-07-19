762d8851c71dee293c15099b92234d544d7ceefe
Tom King <mobilemind@pobox.com>
2017-07-19 15:19:43 -0700

    Gruntfile.js: bump version of OpenInBrave (corrected protocol scheme)

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
       },
       "OpenInBrave": {
         "file": "openinbrave.js",
-        "version": "1.0.0"
+        "version": "1.0.1"
       },
       "OpenInCodeBucket": {
         "file": "openincodebucket.js",
