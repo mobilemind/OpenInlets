ad253b3fbe0559ed7ecbc8e6c096b3630bbe33f3
Tom King <mobilemind@pobox.com>
2017-10-07 23:02:39 -0500

    src/loginesa.txt: re-write for simpler form

--- src/loginesa.js
+++ src/loginesa.js
@@ -1,14 +1,6 @@
 if ('login.innflux.com' === window.location.host) {
-    if (document.getElementById('sRoom')) {
-        document.getElementById('sLastName').value = 'Name';
-        document.getElementById('sRoom').value = '000';
-        document.getElementById('connect').click();
-    } else if (document.getElementById('1')) {
-        document.getElementById('1').checked = true;
-        document.getElementsByName('TOS')[0].checked = true;
-        document.getElementsByName('iStep')[0].click();
-        document.getElementsByTagName('form')[0].submit();
-    }
+    document.getElementById('iAcceptedTerms').checked = true;
+    document.getElementById('button-connect').click();
 } else {
     window.location.href = 'https://login.innflux.com/';
 }
