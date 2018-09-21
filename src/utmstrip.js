49b48618dfb21707e9b9ba931490e5459c9cfc7b
Tom King <mobilemind@pobox.com>
2018-09-20 20:10:54 -0700

    src/utmstrip.js: remove additional common tracking params

--- src/utmstrip.js
+++ src/utmstrip.js
@@ -3,23 +3,45 @@
         let s = window.location.search;
         // Amazon referrals
         if (location.hostname.indexOf("amazon.com") > -1) {
-            s = s.replace(/([?&](_encoding|ie|tag)=[^&#]+)/ig, "");
+            s = s.replace(/[?&](_encoding|ie|psc|ref_|tag)=[^&#]+/ig, "");
+            s = s.replace(/[?&]p[df]_rd_.*?=[^&#]+/ig, "");
+        }
+        // Facebook
+        if (s.indexOf("fb_") > -1) {
+            s = s.replace(/[&?]fb_(?:action_ids|action_types|ref|source)=[^#&]+/ig, "");
+        }
+        if (s.indexOf("action_") > -1) {
+            s = s.replace(/[&?]action_(?:object|ref|type)_map=[^#&]+/ig, "");
+        }
+        // generic
+        if (s.indexOf("Id=") > -1) {
+            s = s.replace(/[&?](?:asset|campaign|elqTrack|recipient|site)Id=[^#&]+/ig, "");
         }
         // Google Analytics
         if (s.indexOf("utm_") > -1) {
-            s = s.replace(/([?&]utm_(campaign|content|medium|reader|source|term)=[^&#]+)/ig, "");
+            s = s.replace(/[&?]utm_(?:campaign|cid|content|design|medium|name|pubreferrer|reader|source|swu|term|viz_id)=[^#&]+/ig, "");
         }
-        // Google YouTube
+        if (s.indexOf("ga_") > -1) {
+            s = s.replace(/[&?]ga_(?:campaign|cid|content|medium|name|place|pubreferrer|source|swu|term|userid|viz_id)=[^#&]+/ig, "");
+        }
+        // Google YouTube # verify w/Patterns
         if (location.hostname.indexOf("youtube.com") > -1) {
-            s = s.replace(/([?&](ac|annotation_id|app|feature|src_vid)=[^&#]+)/ig, "");
+            s = s.replace(/[?&](ac|annotation_id|app|feature|gclid|kw|src_vid)=[^&#]+/ig, "");
         }
         // HubSpot
         if (s.indexOf('_hsenc') > -1 || s.indexOf('_hsmi') > -1) {
-            s = s.replace(/([?&](_hsenc|_hsmi)=[^&#]+)/ig, '');
+            s = s.replace(/[?&](_hsenc|_hsmi)=[^&#]+/ig, '');
+        }
+        if (s.indexOf('hmb_') > -1) {
+            s = s.replace(/[&?]hmb_(?:campaign|medium|source)=[^#&]+/ig, "");
         }
         // MailChimp
         if (s.indexOf('mc_cid') > -1 || s.indexOf('mc_eid') > -1) {
-            s = s.replace(/([?&](mc_cid|mc_eid)=[^&#]+)/ig, '');
+            s = s.replace(/[?&]mc_[ce]id=[^#&]+/ig, '');
+        }
+        // Marketo
+        if (s.indexOf('iesrc') > -1 || s.indexOf('mkt_tok') > -1) {
+            s = s.replace(/[?&](iesrc|mkt_tok)=[^&#]+/ig, '');
         }
         // clean-up
         if (window.location.search !== s) {
