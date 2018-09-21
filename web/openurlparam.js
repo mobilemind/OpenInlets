cf0d6767d55184f96706d4840481b1bee6827540
Tom King <mobilemind@pobox.com>
2018-09-20 20:17:15 -0700

    web/*.js: republish runtimes

--- web/openurlparam.js
+++ web/openurlparam.js
@@ -1,1 +1,1 @@
-javascript:const%20o=location.search.search('url=');if(o%3E-1)%7Blet%20c=location.search.substr(4+o);const%20e=c.indexOf('&');if(e%3E-1)%7Bc=c.substr(0,e)%7Dif(5%3Cc.length)%7Blocation.replace(decodeURIComponent(c))%7D%7Dvoid'1.0.0'
+javascript:const%20e=location.search.search('url=');if(e%3E-1)%7Blet%20o=location.search.substr(4+e);const%20c=o.indexOf('&');if(c%3E-1)%7Bo=o.substr(0,c)%7Dif(5%3Co.length)%7Blocation.replace(decodeURIComponent(o))%7D%7Dvoid'1.0.0'
