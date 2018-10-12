49e26099a0b244951ba2da6957078d37455d68a0
Tom King <mobilemind@pobox.com>
2018-10-12 10:44:11 -0700

    Gruntfile.js: bump version

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
             },
             "UtmStrip": {
                 "file": "utmstrip.js",
-                "version": "1.5.0"
+                "version": "1.5.1"
             }
         },
         "eslint": {
