6a4a0035905bbfc138ab6523bd19026b3041994f
Tom King <mobilemind@pobox.com>
2022-08-30 16:48:00 -0700

    bump modifysupporturl version; update lockfile & republish

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
             },
             "ModifySupportUrl": {
                 "file": "modifysupporturl.js",
-                "version": "1.1.7"
+                "version": "1.2.0"
             },
             "OpenIn1Password": {
                 "file": "openin1password.js",
