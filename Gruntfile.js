fff8a2c98d8d41aafb73d67a9669e44b06c1d6e4
Tom King <mobilemind@pobox.com>
2022-09-08 16:00:25 -0700

    modifysupporturl: republish after fixing static analysis issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
             },
             "ModifySupportUrl": {
                 "file": "modifysupporturl.js",
-                "version": "1.2.4"
+                "version": "1.2.5"
             },
             "OpenIn1Password": {
                 "file": "openin1password.js",
