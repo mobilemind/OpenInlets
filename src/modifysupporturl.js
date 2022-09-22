180d7a8a8c24a452ff3df95b8a981796e07785de
Tom King <mobilemind@pobox.com>
2022-09-22 00:35:03 -0700

    src/*.js: only use strict (in)equality tests vs null

--- src/modifysupporturl.js
+++ src/modifysupporturl.js
@@ -4,7 +4,7 @@
 (() => {
     const docloc = document.location;
     // for now, only work on support.apple.com
-    if ('support.apple.com' !== docloc.host) {
+    if ('support.apple.com' != docloc.host) {
         return;
     }
     const guide = (/(\/guide\/[-0-9a-z]+\/)([-0-9a-z]+)\//),
@@ -48,19 +48,19 @@
         }
 
         // is there a selection range?
-        if ('None' !== selected.type && selected.rangeCount > 0) {
+        if ('None' != selected.type && selected.rangeCount > 0) {
             const range = selected.getRangeAt(0);
             // get DOM node at start of range & go up 1 level if text
             if (range) {
                 const container = range.startContainer;
                 /* eslint-disable-next-line no-ternary, multiline-ternary */
-                const anchor = container.nodeType === 3 ? container.parentNode : container;
+                const anchor = container.nodeType == 3 ? container.parentNode : container;
                 if (anchor.parentNode) {
                     // parent node of a heading is the <div> w/ the anchor id
                     subid = anchor.parentNode.id;
                     heading = anchor.innerText;
                     // update result with id for selection
-                    if (heading !== '' && subid !== '') {
+                    if (heading != '' && subid != '') {
                         linktext = heading;
                         result += '#' + subid;
                     }
@@ -71,5 +71,5 @@
     }
     // show original title & url, heading if selected, and then modified url & markdown
     /* eslint-disable-next-line no-ternary, multiline-ternary */
-    alert(document.title + '\n\n' + url + '\n' + (heading !== '' && subid !== '' ? '\nSelected Heading:\n' + heading + '\n' : '') + '\nModified URL:\n' + result + '\n\nMarkdown link:\n[' + linktext + '](' + result + ')');
+    alert(document.title + '\n\n' + url + '\n' + (heading != '' && subid != '' ? '\nSelected Heading:\n' + heading + '\n' : '') + '\nModified URL:\n' + result + '\n\nMarkdown link:\n[' + linktext + '](' + result + ')');
 })();
