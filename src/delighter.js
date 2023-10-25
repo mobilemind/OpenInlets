db49eb4883c7305172382558021f1e6926e70456
Tom King <mobilemind@pobox.com>
2023-10-24 18:16:27 -0700

    bump package to 3.1.0; update docs & lockfiles; fix typo in comment

--- src/delighter.js
+++ src/delighter.js
@@ -1,5 +1,5 @@
 // deLighter - if the current URL includes a hash, which can imply a text
-// fragment highlight, then reload the page (de-highlight it))
+// fragment highlight, then reload the page (de-highlight it)
 
 if (document.location.href.indexOf('#') > -1) {
     document.location.href = document.location.href.substring(0, document.location.href.indexOf('#'));
