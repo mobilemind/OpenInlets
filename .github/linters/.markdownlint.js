655cb7e124dae07158ba4917b68ea1c40f8f3849
Tom King <mobilemind@pobox.com>
2023-12-25 23:45:39 -0800

    .github/linters/.markdownlint.js: convert to js using aliases

--- .github/linters/.markdownlint.js
+++ .github/linters/.markdownlint.js
@@ -1,12 +1,22 @@
----
-default: true
-MD008: false
-MD013: false
-MD015: false
-MD016: false
-MD017: false
-MD029: false
-MD032: false
-MD033: false
-MD034: false
-MD036: false
+module.exports = {
+    "config": {
+          "default": true,
+          "line-length": false,
+          "ol-prefix": false,
+          "blanks-around-lists": false,
+          "no-inline-html": false,
+          "no-bare-urls": false,
+          "no-emphasis-as-heading": false
+      }
+}
+// default: true
+// MD008: false
+// MD013: false
+// MD015: false
+// MD016: false
+// MD017: false
+// MD029: false
+// MD032: false
+// MD033: false
+// MD034: false
+// MD036: false
