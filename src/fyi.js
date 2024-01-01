e2a256246b02159d1dc1a842b14fa0f9f540f41e
Tom King <mobilemind@pobox.com>
2023-12-31 19:17:01 -0800

    fyi: add from fyi-bookmarklets & streamline

---
+++ src/fyi.js
@@ -0,0 +1,9 @@
+//  fyi - open a new email using page title as the subject, and in the body
+//        put the page tite, URL, and any selected text
+(() => {
+    const pageTitle = encodeURIComponent(document.title),
+        returnChar = '%0A',
+        selected = window.getSelection(),
+        selectedText = 'Range' == selected.type && selected.rangeCount > 0 ? selected.getRangeAt(0).toString() : '';
+    location.href = `mailto:?subject=fyi:${pageTitle}&body=${pageTitle}${returnChar}${encodeURIComponent(document.URL)}${returnChar}---${returnChar}${encodeURIComponent(selectedText)}${returnChar}${returnChar}`;
+})();
