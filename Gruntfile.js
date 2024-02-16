992e8ea0866c2db27c0e5757e4537d7d91e0893c
Tom King <mobilemind@users.noreply.github.com>
2024-02-16 13:10:20 -0800

    x-man: learn to handle man sections; bump version; update docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,7 @@ module.exports = function(grunt) {
             },
             "x-man": {
                 "file": "x-man.js",
-                "version": "1.1.7"
+                "version": "1.1.8"
             }
         },
         "eslint": {
