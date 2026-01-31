8d50423209c2c0b817d594b9a0a241d97ffc846b
Tom King <mobilemind@users.noreply.github.com>
2026-01-31 15:03:54 -0800

    feat(openin*): drop iPod support from openin{app} bookmarklets

--- src/openinbrave.ts
+++ src/openinbrave.ts
@@ -1,4 +1,4 @@
 // verify iOS UserAgent & then prefix URL with protocol of Brave iOS app
-if (/iP(.d|hone)/.test(navigator.userAgent)) {
+if (/iP(ad|hone)/.test(navigator.userAgent)) {
     location.href = `brave://open-url?url=${encodeURIComponent(location.href)}`;
 }
--- src/openinfirefox-private.ts
+++ src/openinfirefox-private.ts
@@ -1,4 +1,4 @@
 // verify iOS UserAgent, prefix URL w/ Firefox iOS protocol, & add private suffix
-if (/iP(.d|hone)/.test(navigator.userAgent)) {
+if (/iP(ad|hone)/.test(navigator.userAgent)) {
     location.href = `firefox://open-url?url=${encodeURIComponent(location.href)}&private=true`;
 }
