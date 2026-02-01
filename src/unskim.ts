f52a45f7902a952e3fb081df393f4cd7f82deb53
Tom King <mobilemind@users.noreply.github.com>
2026-01-31 16:20:54 -0800

    chore(unskim): format changes to source (no functional changes)

--- src/unskim.ts
+++ src/unskim.ts
@@ -5,40 +5,39 @@
 // wrap everything in an anon function for isolation
 
 (() => {
+	// default to getting the current address & making it a URL object to work with
+	let origUrl: URL = new URL(document.location.href);
 
-// default to getting the current address & making it a URL object to work with
-let origUrl: URL = new URL(document.location.href);
+	// handle special case of Safari error page (DNS block of redirection service)
+	if (origUrl.href === 'safari-resource:/ErrorPage.html') {
+		// DO NOT "UNCURL" the apostrophe or quotes in the match regex, that formatting IS CRITICAL
+		const urlStr: string | undefined = document.querySelector('p.error-message')?.textContent?.match(/Safari can't open the page "(https?:[^"]+)"/)?.[1];
+		if (urlStr) {
+			origUrl = new URL(urlStr);
+		}
+	}
 
-// handle special case of Safari error page (DNS block of redirection service)
-if (origUrl.href === 'safari-resource:/ErrorPage.html') {
-  // DO NOT "UNCURL" the apostrophe or quotes in the match regex, that formatting IS CRITICAL
-  const urlStr: string | undefined = document.querySelector('p.error-message')?.textContent?.match(/Safari can't open the page "(https?:[^"]+)"/)?.[1];
-  if (urlStr) {
-    origUrl = new URL(urlStr);
-  }
-}
+	// if the current URL object doesn't even have a searchParam, exit now
+	if (origUrl.search === '') {
+		return;
+	}
 
-// if the current URL object doesn't even have a searchParam, exit now
-if (origUrl.search === '') {
-  return;
-}
+	// create array of common param keys used for URLs (in priority order)
+	const urlKey: string[] = ['url', 'destination', 'redirect', 'target', 'goto', 'u', 'dest', 'link', 'out'],
+				origParams: URLSearchParams = new URLSearchParams(origUrl.search);
 
-// create array of common param keys used for URLs (in priority order)
-const urlKey: string[] = ['url', 'destination', 'redirect', 'target', 'goto', 'u', 'dest', 'link', 'out'],
-      origParams: URLSearchParams = new URLSearchParams(origUrl.search);
+	// find 1st param key match that's a valid looking URL
+	const urlCandidate: string | undefined = urlKey
+		.filter((param: string) => origParams.has(param))
+		.map((param: string) => {
+			const value: string | null = origParams.get(param);
+			return value ? decodeURIComponent(value) : '';
+		})
+		.find((url: string) => url.match(/^https?:/));
 
-// find 1st param key match that's a valid looking URL
-const urlCandidate: string | undefined = urlKey
-  .filter((param: string) => origParams.has(param))
-  .map((param: string) => {
-    const value: string | null = origParams.get(param);
-    return value ? decodeURIComponent(value) : '';
-  })
-  .find((url: string) => url.match(/^https?:/));
-
-// navigate to new URL if found
-if (urlCandidate) {
-  window.location.replace(new URL(urlCandidate));
-}
+	// navigate to new URL if found
+	if (urlCandidate) {
+		window.location.replace(new URL(urlCandidate));
+	}
 
 })();
